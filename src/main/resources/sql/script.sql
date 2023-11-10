ea3b2a4b138c4abbe30fe133a0a03ae0713df11b
Pasindu Yapa <139765303+pasinduy@users.noreply.github.com>
2023-11-10 16:02:54 +0530

    Final Commit

---
+++ src/main/resources/sql/script.sql
@@ -0,0 +1,17 @@
+create database test;
+use test;
+
+create table user(
+    email varchar(35) primary key,
+    username varchar(155) not null,
+    password text not null,
+);
+
+create table tasks(
+    id int primary key auto_increment,
+    email varchar(35) not null,
+    description varchar(255) not null,
+    dueDate date not null,
+    isCompleted boolean default false not null,
+    foreign key (email) references user(email) on delete cascade on update cascade
+)
